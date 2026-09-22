b76c118678f78282f2a37b2f119620b5571040e6
ganesh <ganeshswami1991@gmail.com>
2026-09-22 17:45:10 +0530

    add logging utility for agent pipeline

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -2,11 +2,19 @@ import logging
 import sys
 
 
-def get_logger(name: str = "assistant") -> logging.Logger:
+def setup_logger(name: str = "investment_research", level: int = logging.INFO) -> logging.Logger:
     logger = logging.getLogger(name)
     if not logger.handlers:
+        logger.setLevel(level)
         handler = logging.StreamHandler(sys.stdout)
-        handler.setFormatter(logging.Formatter("[%(asctime)s] %(levelname)s: %(message)s"))
+        handler.setLevel(level)
+        formatter = logging.Formatter(
+            "[%(asctime)s] [%(levelname)s] [%(name)s]: %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        handler.setFormatter(formatter)
         logger.addHandler(handler)
-        logger.setLevel(logging.INFO)
     return logger
+
+
+logger = setup_logger()
